496dd2ab470928b630902da5d819d05cd820ae25
Sávio_pinheiro <94490629+Saviovd@users.noreply.github.com>
2023-01-13 13:43:35 -0300

    Refatorando os arquivos p/ TS

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,37 @@
+import './CampoTexto.css'
+
+interface CampoDeTextoProps {
+    aoAlterado: (valor: string) => void
+    placeholder: string
+    label: string
+    valor: string
+    obrigatorio?: boolean
+    tipo?: 'text' | 'password' | 'email' | 'number' | 'date'
+}
+
+const CampoTexto = ( { aoAlterado, placeholder, label, valor, obrigatorio = false, tipo='text' } : CampoDeTextoProps ) => {
+
+    const placeholderModificada = `${placeholder}...` 
+
+    const aoDigitado = (evento:  React.ChangeEvent<HTMLInputElement>) => {
+        aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {label}
+            </label>
+            <input 
+                value={valor} 
+                onChange={aoDigitado} 
+                required={obrigatorio} 
+                placeholder={placeholderModificada}
+                type={tipo}
+                />
+
+        </div>
+    )
+}
+
+export default CampoTexto
